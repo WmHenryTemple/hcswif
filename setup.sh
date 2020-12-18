8eb6f57be09c2fca55d604653cd690d72544d5e7
William Henry <wmhenry@jlab.org>
2020-12-18 13:06:00 -0500

    don't need to source hcana setup

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ export hcana_setup=/u/group/c-polhe3/software
 export hallc_replay_dir=/group/c-polhe3/Users/$USER/hallc_replay
 # -----------------------------------------------------------------------------
 #  Change if this gives you the wrong version of root, evio, etc
-source /site/12gev_phys/softenv.sh 2.3
+source /site/12gev_phys/softenv.sh 2.1
 source /group/c-polhe3/software/setup.sh
 # -----------------------------------------------------------------------------
 # Source setup scripts
@@ -15,11 +15,11 @@ curdir=`pwd`
 cd $hcana_setup
 source setup.sh
 export PATH=$hcana_dir/bin:$PATH
-echo Sourced $hcana_dir/setup.sh
+echo Sourced $hcana_setup/setup.sh
 
-cd $hallc_replay_dir
-source setup.sh
-echo Sourced $hallc_replay_dir/setup.sh
+#cd $hallc_replay_dir
+#source setup.sh
+#echo Sourced $hallc_replay_dir/setup.sh
 
 echo cd back to $curdir
 cd $curdir
